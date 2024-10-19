9c24e2dc8bcc38690e16a257a1046857561826b7
sunitabhoomi <sunitadengi35@gmail.com>
2024-10-19 20:05:14 +0530

    return 1 if parameter value is '1'

--- string_calculator.js
+++ string_calculator.js
@@ -1,11 +1,15 @@
 function add( string_number){ 
    //input value is empty
-   if(string_number === ''){
-    return 0
+   if (string_number === "") {
+     return 0;
+   }
+   //parameter value is 1
+   else if (string_number.length === 1) {
+     return parseInt(string_number);
    }
   
 }
-add('1,2')
+add('1')
 
 
 module.exports = add;
--- string_calculator.test.js
+++ string_calculator.test.js
@@ -3,3 +3,6 @@ const string_calculator = require("./string_calculator");
 test("should return zero when '' is passed", () => {
   expect(string_calculator("")).toBe(0);
 });
+test('should return one when "1" is passed', () => {
+    expect(string_calculator("1")).toBe(1);
+  });
